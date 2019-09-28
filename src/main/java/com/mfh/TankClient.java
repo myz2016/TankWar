3d481c0f8faef456e9e72a01472eb760c6aafaa8
myz2018_Service <myz2000@yeah.net>
2019-09-28 11:10:59 +0800

    create tankWar project

---
+++ src/main/java/com/mfh/TankClient.java
@@ -0,0 +1,8 @@
+package com.mfh;
+
+/**
+ * @Author mfh
+ * @Date 2019/9/28 11:05
+ */
+public class TankClient {
+}
---
+++ src/test/java/com/mfh/TankClientTest.java
@@ -0,0 +1,8 @@
+package com.mfh;
+
+/**
+ * @Author mfh
+ * @Date 2019/9/28 11:06
+ */
+public class TankClientTest {
+}
