18aec17ebb96604f3a108f7109f515effd4cf682
myz2018_Service <myz2000@yeah.net>
2019-09-28 13:26:22 +0800

    1、添加窗口显示

--- src/main/java/com/mfh/TankClient.java
+++ src/main/java/com/mfh/TankClient.java
@@ -1,8 +1,19 @@
 package com.mfh;
 
+import java.awt.*;
+
 /**
- * @Author mfh
- * @Date 2019/9/28 11:05
+ * @author mfh
+ * @date 2019/9/28 11:05
  */
-public class TankClient {
+public class TankClient extends Frame {
+    private void launchFrame() {
+        this.setLocation(200, 300);
+        this.setSize(800, 600);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        final TankClient client = new TankClient();
+        client.launchFrame();
+    }
 }
