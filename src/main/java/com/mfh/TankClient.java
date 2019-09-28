4993980d5d91b48d7e506c71df2a9609ac234e95
myz2018_Service <myz2000@yeah.net>
2019-09-28 17:19:34 +0800

    1、魔法值使用常量替换并加入注释

--- src/main/java/com/mfh/TankClient.java
+++ src/main/java/com/mfh/TankClient.java
@@ -9,8 +9,19 @@ import java.awt.event.WindowEvent;
  * @date 2019/9/28 11:05
  */
 public class TankClient extends Frame {
-    private int x = 50;
-    private int y = 50;
+    private int tankX = 50;
+    private int tankY = 50;
+    /** 屏幕宽度 */
+    private static final int SCREEN_WIDTH = 800;
+    /** 屏幕高度 */
+    private static final int SCREEN_HEIGHT = 600;
+    private static final int SCREEN_X = 200;
+    private static final int SCREEN_Y = 300;
+    /** 坦克宽度 */
+    private static final int TANK_WIDTH = 30;
+    /** 坦克高度 */
+    private static final int TANK_HEIGHT = 30;
+
     private Image offScreenImage = null;
     public static void main(String[] args) {
         final TankClient client = new TankClient();
@@ -21,21 +32,21 @@ public class TankClient extends Frame {
     public void paint(Graphics g) {
         final Color color = g.getColor();
         g.setColor(Color.YELLOW);
-        g.fillOval(x, y, 30, 30);
+        g.fillOval(tankX, tankY, TANK_WIDTH, TANK_HEIGHT);
         g.setColor(color);
-        y++;
+        tankY++;
     }
 
     @Override
     public void update(Graphics g) {
         if (offScreenImage == null) {
-            offScreenImage = this.createImage(800, 600);
+            offScreenImage = this.createImage(SCREEN_WIDTH, SCREEN_HEIGHT);
         }
         /* 图片上的画笔 */
         final Graphics imageGraphics = offScreenImage.getGraphics();
         final Color color = imageGraphics.getColor();
         imageGraphics.setColor(Color.BLUE);
-        imageGraphics.fillRect(0,0,800,600);
+        imageGraphics.fillRect(0,0, SCREEN_WIDTH, SCREEN_HEIGHT);
         imageGraphics.setColor(color);
         paint(imageGraphics);
 
@@ -44,8 +55,8 @@ public class TankClient extends Frame {
     }
 
     private void launchFrame() {
-        this.setLocation(200, 300);
-        this.setSize(800, 600);
+        this.setLocation(SCREEN_X, SCREEN_Y);
+        this.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
