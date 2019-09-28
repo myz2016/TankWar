cb5a57dedc36b851b0a148d39896c461ab164bbe
myz2018_Service <myz2000@yeah.net>
2019-09-28 21:46:45 +0800

    1、坦克受键盘控制移动

--- src/main/java/com/mfh/TankClient.java
+++ src/main/java/com/mfh/TankClient.java
@@ -1,6 +1,9 @@
 package com.mfh;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -11,18 +14,27 @@ import java.awt.event.WindowEvent;
 public class TankClient extends Frame {
     private int tankX = 50;
     private int tankY = 50;
-    /** 屏幕宽度 */
+    /**
+     * 屏幕宽度
+     */
     private static final int SCREEN_WIDTH = 800;
-    /** 屏幕高度 */
+    /**
+     * 屏幕高度
+     */
     private static final int SCREEN_HEIGHT = 600;
     private static final int SCREEN_X = 200;
     private static final int SCREEN_Y = 300;
-    /** 坦克宽度 */
+    /**
+     * 坦克宽度
+     */
     private static final int TANK_WIDTH = 30;
-    /** 坦克高度 */
+    /**
+     * 坦克高度
+     */
     private static final int TANK_HEIGHT = 30;
 
     private Image offScreenImage = null;
+
     public static void main(String[] args) {
         final TankClient client = new TankClient();
         client.launchFrame();
@@ -34,7 +46,7 @@ public class TankClient extends Frame {
         g.setColor(Color.YELLOW);
         g.fillOval(tankX, tankY, TANK_WIDTH, TANK_HEIGHT);
         g.setColor(color);
-        tankY++;
+//        tankY++;
     }
 
     @Override
@@ -46,7 +58,7 @@ public class TankClient extends Frame {
         final Graphics imageGraphics = offScreenImage.getGraphics();
         final Color color = imageGraphics.getColor();
         imageGraphics.setColor(Color.BLUE);
-        imageGraphics.fillRect(0,0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        imageGraphics.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
         imageGraphics.setColor(color);
         paint(imageGraphics);
 
@@ -61,6 +73,27 @@ public class TankClient extends Frame {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
+            }
+        });
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                final int keyCode = e.getKeyCode();
+                switch (keyCode) {
+                    case KeyEvent.VK_LEFT:
+                        tankX -= 5;
+                        break;
+                    case KeyEvent.VK_UP:
+                        tankY -= 5;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        tankX += 5;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        tankY += 5;
+                        break;
+                    default:
+                }
             }
         });
         this.setBackground(Color.BLUE);
